2ea86f2ed63a3da9a3ecc19cc65169bcecfbf033
22jy0109 <22jy0109@PC23.jynet2.jec.ac.jp>
2023-09-27 11:40:19 +0900

    HelloWorld12345を3回表示

--- src/JavaProject/HelloWorld.java
+++ src/JavaProject/HelloWorld.java
@@ -4,8 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		for(int i=0;i < 3;i++) {
 			System.out.println("HelloWorld12345");
-			
+		}
 	}
 
 }
